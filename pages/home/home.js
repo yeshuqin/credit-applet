45dd6eed6f0c4413d0515990b2d059863f735fe4
YeShuqin <n009229@archermind.com>
2018-04-03 16:08:49 +0800

    样式

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,7 +7,26 @@ Page({
   data: {
     userInfo:{
       name:'叶淑琴'
-    }
+    },
+    array: ['设为已还', '设为未还'],
+    objectArray: [
+      {
+        id: 0,
+        name: '设为已还'
+      },
+      {
+        id: 1,
+        name: '设为未还'
+      }
+    ],
+    index: 0,
+  },
+
+  bindPickerChange:function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
   },
 
   /**
